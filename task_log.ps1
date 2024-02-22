3d1964a08e5112135dbe2ee7478bc44a6fb6b6b4
Antoine <61878841+AgeLastX@users.noreply.github.com>
2024-02-22 01:25:16 +0100

    Event planner

---
+++ task_log.ps1
@@ -0,0 +1,11 @@
+﻿$scriptPath = "C:\Scripts\Extrac log.ps1"
+$taskAction = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument $scriptPath
+$taskTrigger = New-ScheduledTaskTrigger -Daily -At 9am
+$taskUser = "NT AUTHORITY\SYSTEM"
+$taskName = "Extraclog"
+
+if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
+} else {
+    Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger -User $taskUser -TaskName $taskName -Force
+}
+
---
+++ task_memory.ps1
@@ -0,0 +1,10 @@
+﻿$scriptPath = "C:\Scripts\Memory_dump.ps1"
+$taskAction = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument $scriptPath
+$taskTrigger = New-ScheduledTaskTrigger -At 9am -Once -RepetitionDuration (New-TimeSpan -Days 999) -RepetitionInterval (New-TimeSpan -Hours 1)
+$taskUser = "NT AUTHORITY\SYSTEM"
+$taskName = "Dump Memory"
+
+if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
+} else {
+    Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger -User $taskUser -TaskName $taskName -Force
+}
