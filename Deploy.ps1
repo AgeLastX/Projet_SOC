c8623431a335092aeb5a38e75093c1cda7b66ac9
Antoine <61878841+AgeLastX@users.noreply.github.com>
2024-02-22 02:14:00 +0100

    Add files via upload

---
+++ Deploy.ps1
@@ -0,0 +1,23 @@
+﻿$dossierScriptsBureau = "C:\Users\$env:USERNAME\Bureau\Projet_SOC-main"
+
+$nouvelEmplacement = "C:\"
+
+Move-Item -Path $dossierScriptsBureau -Destination $nouvelEmplacement -Force
+
+$dossierDeplace = Join-Path $nouvelEmplacement "Projet_SOC-main"
+
+Rename-Item -Path $dossierDeplace -NewName "Scripts" -Force
+
+
+#ENROLEMENT
+$script1 = "C:\Scripts\enrole.ps1"
+#Logs Extract
+$script2 = "C:\Scripts\Extrac log.ps1"
+#Dump memory
+$script3 = "C:\Scripts\Script2.ps1"
+
+Set-Location -Path "C:\Scripts"
+
+& $script1
+& $script2
+& $script3
---
+++ enrol.ps1
@@ -0,0 +1,5 @@
+﻿$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -Uri https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.12.1-windows-x86_64.zip -OutFile elastic-agent-8.12.1-windows-x86_64.zip
+Expand-Archive .\elastic-agent-8.12.1-windows-x86_64.zip -DestinationPath .
+cd elastic-agent-8.12.1-windows-x86_64
+.\elastic-agent.exe install --url=https://7a816ee180f54b31b165948c49b65122.fleet.us-central1.gcp.cloud.es.io:443 --enrollment-token=MGk5VnpvMEJDa3NIUUZadWFtZEg6aTdrd0JENkFRbWFZQ1ZvVld6RzJnQQ==
